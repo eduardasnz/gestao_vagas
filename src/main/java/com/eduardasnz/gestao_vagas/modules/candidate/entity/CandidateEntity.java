440f6d10cd5d7172e0147388774fb4d77938ea01
Maria Eduarda <meduardasan18@hotmail.com>
2026-07-10 18:58:05 -0300

    feat: add examples in schemas

--- src/main/java/com/eduardasnz/gestao_vagas/modules/candidate/entity/CandidateEntity.java
+++ src/main/java/com/eduardasnz/gestao_vagas/modules/candidate/entity/CandidateEntity.java
@@ -6,6 +6,7 @@ import java.util.UUID;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.validator.constraints.Length;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -21,16 +22,19 @@ public class CandidateEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
-
+    @Schema(example = "John Doe")
     private String name;
 
     @Pattern(regexp = "^\\S+$", message = "Campo [USERNAME] não deve conter espaço.")
+    @Schema(example = "johndoe10")
     private String username;
     
     @Email(message = "Campo [EMAIL] deve conter email válido.")
+    @Schema(example = "johndoe@example.com")
     private String email;
 
     @Length(min=1, max=80)
+    @Schema(example = "SenhaSecreta012@")
     private String password;
     private String curriculum;
 
