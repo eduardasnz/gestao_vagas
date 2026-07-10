96cdbc321cb31e9d5df8cb4d0997918dfbc1a1e4
Maria Eduarda <meduardasan18@hotmail.com>
2026-07-10 17:35:32 -0300

    feat: add entities (candidate, company and job)

---
+++ src/main/java/com/eduardasnz/gestao_vagas/modules/candidate/entity/CandidateEntity.java
@@ -0,0 +1,40 @@
+package com.eduardasnz.gestao_vagas.modules.candidate.entity;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.validator.constraints.Length;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
+import lombok.Data;
+
+@Data
+@Entity(name = "Candidate")
+public class CandidateEntity {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    private String name;
+
+    @Pattern(regexp = "^\\S+$", message = "Campo [USERNAME] não deve conter espaço.")
+    private String username;
+    
+    @Email(message = "Campo [EMAIL] deve conter email válido.")
+    private String email;
+
+    @Length(min=1, max=80)
+    private String password;
+    private String curriculum;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+}
