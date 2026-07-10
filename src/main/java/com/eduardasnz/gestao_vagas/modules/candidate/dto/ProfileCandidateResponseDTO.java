d1e179cb3fe250c8d8753f9fcfffaeefce35245c
Maria Eduarda <meduardasan18@hotmail.com>
2026-07-10 17:39:12 -0300

    feat: add DTO's

---
+++ src/main/java/com/eduardasnz/gestao_vagas/modules/candidate/dto/ProfileCandidateResponseDTO.java
@@ -0,0 +1,20 @@
+package com.eduardasnz.gestao_vagas.modules.candidate.dto;
+
+import java.util.UUID;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProfileCandidateResponseDTO {
+    
+    private String name;
+    private String username;
+    private String email;
+    private UUID id;
+}
---
+++ src/main/java/com/eduardasnz/gestao_vagas/modules/company/dtos/AuthCompanyResponseDTO.java
@@ -0,0 +1,15 @@
+package com.eduardasnz.gestao_vagas.modules.company.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthCompanyResponseDTO {
+    private String access_token;
+    private Long expires_in;
+}
