30ddf951d5be643aa77dca42ac13339015975332
Maria Eduarda <meduardasan18@hotmail.com>
2026-07-14 13:41:45 -0300

    feat: add builder and constructor in dto + entity

--- src/main/java/com/eduardasnz/gestao_vagas/modules/company/dtos/CreateJobDTO.java
+++ src/main/java/com/eduardasnz/gestao_vagas/modules/company/dtos/CreateJobDTO.java
@@ -1,9 +1,15 @@
 package com.eduardasnz.gestao_vagas.modules.company.dtos;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class CreateJobDTO {
 
     @Schema(example = "Desenvolvedor Full Stack para atuar na evolução de nossa plataforma de e-commerce e arquitetura de microsserviços.")
--- src/main/java/com/eduardasnz/gestao_vagas/modules/company/entities/CompanyEntity.java
+++ src/main/java/com/eduardasnz/gestao_vagas/modules/company/entities/CompanyEntity.java
@@ -14,10 +14,16 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Entity(name = "Company")
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class CompanyEntity {
 
     @Id
