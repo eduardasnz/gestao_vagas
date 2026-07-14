e113fb696036c59cc4416a1461c557471ee1503d
Maria Eduarda <meduardasan18@hotmail.com>
2026-07-14 14:40:46 -0300

    feat: refactor code

--- src/main/java/com/eduardasnz/gestao_vagas/modules/candidate/services/ProfileCandidateService.java
+++ src/main/java/com/eduardasnz/gestao_vagas/modules/candidate/services/ProfileCandidateService.java
@@ -3,9 +3,9 @@ package com.eduardasnz.gestao_vagas.modules.candidate.services;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import com.eduardasnz.gestao_vagas.expections.UserNotFoundExpection;
 import com.eduardasnz.gestao_vagas.modules.candidate.dto.ProfileCandidateResponseDTO;
 import com.eduardasnz.gestao_vagas.modules.candidate.repository.CandidateRepository;
 
@@ -17,7 +17,7 @@ public class ProfileCandidateService {
 
     public ProfileCandidateResponseDTO execute(UUID idCandidate) {
         var candidate = this.candidateRepository.findById(idCandidate).orElseThrow(() -> {
-            throw new UsernameNotFoundException("Usuário não existe.");
+            throw new UserNotFoundExpection();
         });
 
         var candidateDTO = ProfileCandidateResponseDTO.builder().name(candidate.getName()).username(candidate.getUsername()).email(candidate.getEmail())
